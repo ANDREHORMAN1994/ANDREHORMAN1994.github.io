d773e564a42e50b18f7c4922726dbc042c5dad9b
André Horman de Medeiros Correia <tavernamoderna8000@gmail.com>
2020-10-16 13:50:24 -0300

    server rodando no backend - Projeto HAPPY - 16/10/2020

--- NLW/Projeto-Happy/src/server.js
+++ NLW/Projeto-Happy/src/server.js
@@ -1,13 +1,25 @@
 // importar dependência
 const express = require('express');
+const path = require('path');
 
 // iniciando lib express
 const server = express();
 
+// declarando onde estão os arquivos estáticos
+server.use(express.static('public'))
+
 // criar uma rota
-server.get('/', (request, response) => {
-  return response.send('oi');
+.get('/', (request, response) => {
+  // console.log(path.join(__dirname, 'views', 'index.html'));
+  return response.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
 // ligar o servidor na porta
 server.listen(5500);
+
+/* ATENÇÃO : 
+O nodemon pode crashar, Erro: Error: listen EADDRINUSE :::5500
+Significa que a porta já está em uso, para resolver isso você pode encerrar o processo que está rodando nessa porta executando o comando:
+
+  pkill node
+*/
